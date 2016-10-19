147b3033be1d87b1c8177871bcae357bc6136621
Shane Thompson <shanet80@gmail.com>
2016-10-18 22:14:21 -0400

    Complete Regression
    Complete regression lession

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -17,6 +17,7 @@ import sys
 import pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn import linear_model
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
 
 ### list the features you want to look at--first item in the 
@@ -29,7 +30,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,8 +38,13 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+reg = linear_model.LinearRegression()
+reg.fit(feature_train, target_train)
 
-
+print "Slope of linear regresssion line is {}".format(reg.coef_)
+print "Intercept of regression line is {}".format(reg.intercept_)
+print "Prediction score on training data: {}".format(reg.score(feature_train, target_train))
+print "Prediction score on test data: {}".format(reg.score(feature_test, target_test))
 
 
 
@@ -64,7 +70,24 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+print "Slope of linear regresssion line is {}".format(reg.coef_)
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
 plt.show()
+
+
+features_list = ["bonus", "long_term_incentive"]
+data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
+target, features = targetFeatureSplit( data )
+feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
+
+regr = linear_model.LinearRegression()
+regr.fit(feature_train, target_train)
+
+print ""
+print "Slope of linear regresssion line is {}".format(regr.coef_)
+print "Intercept of regression line is {}".format(regr.intercept_)
+print "Prediction score on test data: {}".format(regr.score(feature_test, target_test))
