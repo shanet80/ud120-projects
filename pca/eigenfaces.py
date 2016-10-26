b60d0b58ba1a0e88b1cd2393246d7ee8ca87194b
Shane Thompson <shanet80@gmail.com>
2016-10-26 13:28:57 -0400

    Complete PCA
    Complete PCA section

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -73,6 +73,12 @@ t0 = time()
 pca = RandomizedPCA(n_components=n_components, whiten=True).fit(X_train)
 print "done in %0.3fs" % (time() - t0)
 
+### Print PCA explained_variance_ratio_ for first two components
+print ""
+print "Variance from first component: {}".format(pca.explained_variance_ratio_[0])
+print "Variance from second component: {}".format(pca.explained_variance_ratio_[1])
+print ""
+
 eigenfaces = pca.components_.reshape((n_components, h, w))
 
 print "Projecting the input data on the eigenfaces orthonormal basis"
@@ -92,7 +98,7 @@ param_grid = {
           'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1],
           }
 # for sklearn version 0.16 or prior, the class_weight parameter value is 'auto'
-clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
+clf = GridSearchCV(SVC(kernel='rbf', class_weight='auto'), param_grid)
 clf = clf.fit(X_train_pca, y_train)
 print "done in %0.3fs" % (time() - t0)
 print "Best estimator found by grid search:"
