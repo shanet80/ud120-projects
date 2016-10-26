da3cda4712894db63c667c05ec44877e0994ceee
Shane Thompson <shanet80@gmail.com>
2016-10-26 13:01:32 -0400

    Complete Text Learning and Feature Selection
    Completed Text Learning and Feature Selection sections

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -8,7 +8,7 @@ numpy.random.seed(42)
 ### The words (features) and authors (labels), already largely processed.
 ### These files should have been created from the previous (Lesson 10)
 ### mini-project.
-words_file = "../text_learning/your_word_data.pkl" 
+words_file = "../text_learning/your_word_data.pkl"
 authors_file = "../text_learning/your_email_authors.pkl"
 word_data = pickle.load( open(words_file, "r"))
 authors = pickle.load( open(authors_file, "r") )
@@ -39,5 +39,16 @@ labels_train   = labels_train[:150]
 
 ### your code goes here
 
+print "Number of training points: {}".format(len(features_train))
 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+accuracy = accuracy_score(pred, labels_test)
+print "Accuracy Score : {}".format(accuracy)
 
+importances = [i for i in clf.feature_importances_ if i>0.2]
+print importances
+print vectorizer.get_feature_names()[list(clf.feature_importances_).index(importances[1])]
